789cd0e957e8e59a0ed09e9fbeb60fb0dcb66376
BusKetZz <busketz2k@gmail.com>
2020-08-02 21:50:08 +0200

    server/include/communication.h: cleanup

--- server/include/communication.h
+++ server/include/communication.h
@@ -23,10 +23,10 @@
 /* PUBLIC FUNCTIONS PROTOTYPES */
 /*****************************************************************************/
 
+status_t allocate_mapping(modbus_mapping_t *modbus_mapping);
+
 status_t communication_handler(modbus_t *modbus_context, uint8_t *request,
     modbus_mapping_t *modbus_mapping);
-
-status_t allocate_mapping(modbus_mapping_t *modbus_mapping);
 
 #endif /* COMMUNICATION_H */
 
