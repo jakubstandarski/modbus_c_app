34242baceb60668799d0edc31dfcb3dd36c4d4ee
BusKetZz <busketz2k@gmail.com>
2020-08-02 21:00:18 +0200

    server/source/modbus_server.c: preliminary implementation of "main()" function added

--- server/source/modbus_server.c
+++ server/source/modbus_server.c
@@ -18,9 +18,27 @@
 
 
 
+/*****************************************************************************/
+/* MAIN */
+/*****************************************************************************/
+
 int main(void)
 {
+    modbus_t *modbus_context = NULL;
+    const char ip_address[] = "127.0.0.1";
+    int tcp_port = 1502;
+    int incoming_connections_max = 1;
 
+    status_t status;
+    status = tcp_establish_connection(modbus_context, ip_address, tcp_port,
+        incoming_connections_max);
+    if (status != status_success) {
+        modbus_free(modbus_context);
+        return -1;
+    }
+
+    modbus_close(modbus_context);
+    modbus_free(modbus_context);
     return 0;
 }
 
