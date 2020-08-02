3ab36b1bd44a76ae34e47ef0948b1c681ea52f1d
BusKetZz <busketz2k@gmail.com>
2020-08-02 20:29:05 +0200

    server/source/modbus_server.c: header files added

--- server/source/modbus_server.c
+++ server/source/modbus_server.c
@@ -1,3 +1,22 @@
+/*
+ * Author: Jakub Standarski
+ * Date: 02.08.2020
+ *
+ */
+
+
+
+/*****************************************************************************/
+/* HEADER */
+/*****************************************************************************/
+
+#include "tcp_connection.h"
+
+#include <errno.h>
+#include <modbus.h>
+#include <stdio.h>
+
+
 
 int main(void)
 {
