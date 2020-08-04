0619d41d0858c8ace383041f6988e9aa1f0f4594
BusKetZz <busketz2k@gmail.com>
2020-08-04 13:51:11 +0200

    client/source/modbus_client.c: preliminary version of modbus client application to test whether connection between client and server works properly

--- client/source/modbus_client.c
+++ client/source/modbus_client.c
@@ -27,14 +27,27 @@ int main(void)
 {
     modbus_t *modbus_context = NULL;
     const char ip_address[] = "127.0.0.1";
-    int tcp_port = 1502;
-    
+
+    int tcp_port = 0;
+    printf("Input TCP port number: ");
+    scanf("%d", &tcp_port);
+
     status_t status;
     status = tcp_establish_connection(modbus_context, ip_address, tcp_port);
     if (status != status_success) {
         modbus_free(modbus_context);
         return -1;
     }
+
+    uint16_t registers_table[MODBUS_MAX_READ_REGISTERS] = {0};
+    int registers_read_count = -1;
+    registers_read_count = modbus_read_registers(modbus_context, 0,
+        MODBUS_MAX_READ_REGISTERS, registers_table);
+    if (registers_read_count == -1) {
+        fprintf(stderr, "Reading registers failed: %s\n",
+            modbus_strerror(errno));
+    }
+    printf("Register value: %u\n", registers_table[0]);
     
     modbus_close(modbus_context);
     modbus_free(modbus_context);
