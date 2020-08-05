bb8f4f7c7b662021569a74f880079e4a26ff04a8
BusKetZz <busketz2k@gmail.com>
2020-08-05 21:45:04 +0200

    client/source/modbus_client.c: client application made to work in infinite loop

--- client/source/modbus_client.c
+++ client/source/modbus_client.c
@@ -11,6 +11,7 @@
 /*****************************************************************************/
 
 #include <errno.h>
+#include <ctype.h>
 #include <modbus.h>
 #include <stdio.h>
 
@@ -32,54 +33,72 @@ static int get_registers_count(int registers_start_address);
 int main(void)
 {
     const char ip_address[] = "127.0.0.1";
-
-    int tcp_port = 0;
-    printf("Input TCP port number: ");
-    scanf("%d", &tcp_port);
-
     modbus_t *modbus_context = NULL;
-    modbus_context = modbus_new_tcp(ip_address, tcp_port);
-    if (modbus_context == NULL) {
-        fprintf(stderr, "Initializing modbus context failed: %s\n",
-            modbus_strerror(errno));
-        return -1;
-    }
 
-    int error_code = -1;
-    error_code = modbus_connect(modbus_context);
-    if (error_code == -1) {
-        fprintf(stderr, "Connection establishment failed: %s\n",
-            modbus_strerror(errno));
-        modbus_free(modbus_context);
-        return -1;
-    }
-    printf("Connection established successfully:\t");
-    printf("IP address: %s\t TCP port: %d\n", ip_address, tcp_port);
+    char state = 'Y';
+    while (state == 'Y') {
+
+        int tcp_port = 0;
+        printf("\nInput TCP port number: ");
+        scanf("%d", &tcp_port);
+
+        modbus_context = modbus_new_tcp(ip_address, tcp_port);
+        if (modbus_context == NULL) {
+            fprintf(stderr, "Initializing modbus context failed: %s\n",
+                modbus_strerror(errno));
+            return -1;
+        }
+
+        int error_code = -1;
+        error_code = modbus_connect(modbus_context);
+        if (error_code == -1) {
+            fprintf(stderr, "Connection establishment failed: %s\n",
+                modbus_strerror(errno));
+            modbus_free(modbus_context);
+            return -1;
+        }
+        printf("Connection established successfully:\t");
+        printf("IP address: %s\t TCP port: %d\n", ip_address, tcp_port);
 
 
-    int registers_start_address = 0;
-    registers_start_address = get_registers_start_address();
+        int registers_start_address = 0;
+        registers_start_address = get_registers_start_address();
 
-    int registers_count = 0;
-    registers_count = get_registers_count(registers_start_address);
+        int registers_count = 0;
+        registers_count = get_registers_count(registers_start_address);
 
-    uint16_t registers_table[MODBUS_MAX_READ_REGISTERS] = {0};
-    int registers_read_count = -1;
-    registers_read_count = modbus_read_registers(modbus_context,
-        registers_start_address, registers_count, registers_table);
-    if (registers_read_count == -1) {
-        fprintf(stderr, "Reading registers failed: %s\n",
-            modbus_strerror(errno));
-        modbus_close(modbus_context);
-        modbus_free(modbus_context);
-        return -1;
-    }
+        uint16_t registers_table[MODBUS_MAX_READ_REGISTERS] = {0};
+        int registers_read_count = -1;
+        registers_read_count = modbus_read_registers(modbus_context,
+            registers_start_address, registers_count, registers_table);
+        if (registers_read_count == -1) {
+            fprintf(stderr, "Reading registers failed: %s\n",
+                modbus_strerror(errno));
+            modbus_close(modbus_context);
+            modbus_free(modbus_context);
+            return -1;
+        }
 
-    for (int register_index = 0; register_index < registers_count;
-        register_index++) {
-        printf("Register[%d] value: %u\n", register_index +
-            registers_start_address, registers_table[register_index]);
-    }
+
+        for (int register_index = 0; register_index < registers_count;
+            register_index++) {
+            printf("Register[%d] value: %u\n", register_index +
+                registers_start_address, registers_table[register_index]);
+        }
+
+
+        while (1) {
+            printf("\nDo you want to continue? [Y/N]: ");
+            scanf(" %c", &state);
+            state = toupper(state);
+            if (state == 'Y' || state == 'N') {
+                break;
+            } else {
+                fprintf(stderr, "Invalid character...\n");
+            }
+        }
+    } 
+
     
     modbus_close(modbus_context);
     modbus_free(modbus_context);
